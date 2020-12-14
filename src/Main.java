718f890ec879eb8bc4d37df5dbf56f2878c3f3fa
AlbertZasura <albertalbert841@gmail.com>
2020-12-14 16:36:26 +0700

    update promo

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,13 @@
 
-import controller.AdminController;
-import controller.AuthController;
 import controller.LoginController;
-import controller.ProductController;
-import controller.PromoController;
-import controller.RegisterController;
-
-import view.CustomerHomeView;
-import view.HireStaffView;
-import view.LoginView;
-import view.RegisterView;
 
 
 public class Main {
 
 	public Main() {
-		AdminController.getInstance().view().showForm();
+//		AdminController.getInstance().view().showForm();
 //		RegisterController.getInstance().view().showForm();
-//		LoginController.getInstance().view().showForm();
+		LoginController.getInstance().view().showForm();
 //		AuthController.getInstance().view().showForm();
 		
 //		new RegisterView().showForm();
